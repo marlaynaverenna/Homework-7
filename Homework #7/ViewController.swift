c9e522ed45367d246741c3d50da05ebd6ea7b25b
Marlayna Verenna <m.verenna@setonhill.edu>
2020-04-06 13:10:45 -0400

    Initial Commit

---
+++ Homework #7/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Homework #7
+//
+//  Created by Marlayna Verenna on 4/6/20.
+//  Copyright © 2020 Marlayna Verenna. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    let locMan: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    
+    // Disney Coordinates
+    let disneyLatitude: CLLocationDegrees = 28.3852
+    let disneyLongitude: CLLocationDegrees = -81.5639
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let newLocation: CLLocation=locations[0]
+        NSLog("Somthing is happening")
+        
+        // horizontal accuracy less than 0 means failure at gps level
+        if newLocation.horizontalAccuracy >= 0 {
+            
+            let disney:CLLocation = CLLocation(latitude: disneyLatitude, longitude: disneyLongitude)
+            
+            let delta:CLLocationDistance = disney.distance(from: newLocation)
+            
+            let miles: Double = (delta * 0.000621371) + 0.5
+              // meters to rounded miles
+            
+            if miles < 3 {
+                // Stop updating the location
+                locMan.stopUpdatingLocation()
+                // Congratulate the user
+                distanceLabel.text = "Welcome to\nDisney World!"
+            } else {
+                let commaDelimited: NumberFormatter = NumberFormatter()
+                commaDelimited.numberStyle = NumberFormatter.Style.decimal
+                
+              distanceLabel.text=commaDelimited.string(from: NSNumber(value: miles))! + " miles"
+            }
+            
+            
+        }
+        else {
+            // add action if error with GPS
+        }
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        locMan.delegate = self
+        locMan.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locMan.distanceFilter = 1609; // a mile (in meters)
+        locMan.requestWhenInUseAuthorization() // verify access to gps
+        locMan.startUpdatingLocation()
+        startLocation = nil
+    }
+
+
+}
+
